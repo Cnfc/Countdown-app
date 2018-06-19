c97fc45cd1fd0614b70fd14b00ec1a0e24cd3869
cnfc <stdashkov@mail.ru>
2018-06-19 18:38:59 +0300

    Clock components test

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
-var {Link, IndexLink} = require('react-router');
-
 
 
 var Clock = React.createClass({
+  formatSeconds: function (totlalSeconds) {
+    var seconds = totlalSeconds % 60;
+    var minuts = Math.floor(totlalSeconds / 60);
+
+    if(seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+    if(minuts < 10) {
+      minuts = '0' + minuts;
+    }
+
+    return minuts + ':' + seconds;
+  },
   render:function () {
     return <p>clocks</p>
     }
